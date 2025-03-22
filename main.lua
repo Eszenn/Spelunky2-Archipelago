8e99e3a53999502d18c628be850e31ca5e0c8005
Eszenn <heinrichs.ericb@gmail.com>
2025-03-22 14:22:05 -0600

    Update version

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ meta = {
     name = "Spelunky 2 Archipelago",
     description = "Adds Archipelago Multiworld Randomizer support!",
     author = "Eszenn",
-    version = "0.1.1",
+    version = "0.1.2",
     unsafe = true
 }
 
